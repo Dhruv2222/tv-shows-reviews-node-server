e54ae6f8f4be466b311c324f5db2b3399cf8a995
deril2605 <derilraju@gmail.com>
2024-04-02 19:19:26 -0400

    routes for profile

--- Users/dao.js
+++ Users/dao.js
@@ -1,9 +1,33 @@
 import userModel from "./model.js";
 export const findAllUsers = () => userModel.find();
-export const findUserById = (id) => userModel.findById();
+export const findUserById = (id) => userModel.findById(id);
 export const createUser = (user) => userModel.create(user);
-export const findUserByUsername = (username) =>  userModel.findOne({ username: username });
-export const findUserByCredentials = (username, password) =>  userModel.findOne({ username, password });
-export const updateUser = (userId, user) =>  userModel.updateOne({ _id: userId }, { $set: user });
+export const findUserByUsername = (username) => userModel.findOne({ username: username });
+export const findUserByCredentials = (username, password) => userModel.findOne({ username, password });
+export const updateUser = (userId, user) => userModel.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => userModel.deleteOne({ _id: userId });
-export const findUserByEmail = (email) => userModel.findOne({email:email,});
+export const findUserByEmail = (email) => userModel.findOne({ email: email, });
+
+export const getReviewsByUserId = (userId) => userModel.aggregate([
+    {
+        $match: {
+            _id: userId
+        }
+    },
+    {
+        $lookup: {
+            from: "reviews-table",
+            localField: "_id",
+            foreignField: "user_id",
+            as: "reviews"
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            reviews: 1
+        }
+    },
+])
+
+
--- Users/schema.js
+++ Users/schema.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 export default mongoose.Schema(
-    {id: String,
-username: {type: String, unique: true, required: true},
-password: {type: String, required: true},
-email: String,
-phone_number: String,
-role: String,
-favorite_TVshow: String},
-{collection : "users-table"});
+    {
+        _id: String,
+        username: { type: String, unique: true, required: true },
+        password: { type: String, required: true },
+        email: String,
+        phone_number: String,
+        role: String,
+        favorite_TVshow: String
+    },
+    { collection: "users-table" });
