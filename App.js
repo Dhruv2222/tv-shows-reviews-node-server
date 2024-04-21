66f6012b0e4b0d84cf68dd8f7c51b6a541222793
deril2605 <derilraju@gmail.com>
2024-04-21 11:53:31 -0400

    changes for render.com deploy

--- App.js
+++ App.js
@@ -9,12 +9,76 @@ import WishlistRoutes from "./WishList/routes.js";
 import mongoose from "mongoose";
 import SearchRoutes from "./Search/routes.js";
 import ShowRoutes from "./Show/routes.js";
+import "dotenv/config";
 
 
+const app = express();
+app.use(express.json());
 
+// const sessionOptions = {
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+//   proxy: true,
+//   cookie: {
+//     sameSite: "none",
+//     secure: true,
+//     domain: "kanbas-node-server-app-1-mds9.onrender.com",
+//   },
+// };
 
+// app.use(
+//   session(sessionOptions)
+// );
 
-mongoose.connect("mongodb+srv://Cluster89442:aXF2Zm56TlRE@cluster89442.0fq4prg.mongodb.net/sample-d");
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+console.log("Above-", process.env.NODE_ENV);
+if (process.env.NODE_ENV !== "development") {
+  console.log("non dev env");
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+
+}
+app.use(session(sessionOptions));
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000", // Allow requests from this origin
+//     credentials: true, // Allow cookies and HTTP authentication to be sent
+//   })
+// );
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+// app.use(
+//   session({
+//     secret: "pizza is best",
+//     // cookie: {secure: true} // use when deployed remotely
+//   })
+// );
+ShowRoutes(app);
+WishlistRoutes(app);
+ReviewRoutes(app);
+AuthRoutes(app);
+SearchRoutes(app);
+UserRoutes(app);
+Hello(app);
+
+// mongoose.connect("mongodb+srv://Cluster89442:aXF2Zm56TlRE@cluster89442.0fq4prg.mongodb.net/sample-d");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+mongoose.connect(CONNECTION_STRING);
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
 });
@@ -27,47 +91,5 @@ mongoose.connection.on("disconnected", () => {
   console.log("Disconnected from MongoDB");
 });
 
-const assignmentSchema = new mongoose.Schema({
-  _id: String,
-  title: String,
-  course: String,
-});
 
-// Create a model
-const Assignment = mongoose.model("Assignment", assignmentSchema, "s");
-
-// Define an async function to run the query
-async function retrieveAssignments() {
-  try {
-    // Run the query
-    const assignments = await Assignment.find({ course: "RS103" }).exec();
-    console.log("Assignments for RS103:", assignments);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-retrieveAssignments();
-
-const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
-    credentials: true, // Allow cookies and HTTP authentication to be sent
-  })
-);
-app.use(express.json());
-app.use(
-  session({
-    secret: "pizza is best",
-    // cookie: {secure: true} // use when deployed remotely
-  })
-);
-ShowRoutes(app);
-WishlistRoutes(app);
-ReviewRoutes(app);
-AuthRoutes(app);
-SearchRoutes(app);
-UserRoutes(app);
-Hello(app);
 app.listen(process.env.PORT || 4000);
